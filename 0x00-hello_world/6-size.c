3caa085b048a00862c4d447afd83721667479278
Clare <awuorit24@gmail.com>
2023-02-16 10:29:11 -0800

    tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
+
 	int a;
 	long int b;
 	long long int c;
 	char d;
 	float f;
+
 
 	print f("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	print("Size of an int: %lu bytr(s)\n", (unsigned long)sizeof(a));
